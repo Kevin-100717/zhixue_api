bc71a21b602aa7f667e702f6494cdcf33f284bc3
hWater0 <guadaba0512@outlook.com>
2026-06-14 21:04:49 +0800

    处理冲突-P2

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from getData import ZhixueWeb
 from answer_conv import AnswerToImage
+import getpass
 if __name__ == "__main__":
-    us,ps = (input("USERNAME="),input("PASSWORD="))
-    z = ZhixueWeb(us,ps)
+    z = ZhixueWeb()
+    if z.account.getLoginState(False) != "success":
+        print("请先登录")
+        us = input("USERNAME=")
+        ps = getpass.getpass("PASSWORD=")
+        z.account.login(us, ps)
+        if z.account.getLoginState() != "success":
+            print("登录失败")
+            exit()
+    z.init()
     print("选择学年")
     i = 0
     for y in z.years:
